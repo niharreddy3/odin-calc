689f95184f0c3861a43db0f04b538ca7ceae390b
Nihar Reddy <niharreddy3@gmail.com>
2024-08-04 21:25:03 -0500

    update display when num pad buttons pressed

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let firstNumber;
 let operator;
 let secondNumber;
+let displayValue = [];
+
+// display the selected buttons on the calc's display.
+
+let display = document.querySelector(".calc-display");
 
 function add(num1, num2) {
   return num1 + num2;
@@ -32,3 +37,26 @@ function operate(op, num1, num2) {
       return divide(operation, num1, num2);
   }
 }
+
+function updateDisplay(btnChar) {
+  if (btnChar === "clear") {
+    firstNumber = null; 
+    operator = null; 
+    secondNumber = null;
+    display.textContent = "";
+  } else if (btnChar === "=") {
+    let result = operate(operator, firstNumber, secondNumber); 
+    display.textContent = result;
+  } else {
+    displayValue.push(btnChar);
+    display.textContent = displayValue.join(" ");
+  }
+}
+
+let numPad = document.querySelector(".num-pad");
+
+numPad.addEventListener("click", (event) => {
+  let target = event.target;
+
+  updateDisplay(target.textContent);
+});
